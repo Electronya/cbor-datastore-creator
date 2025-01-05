751bdf5a4de93b1fc463771820c76bbf08806207
jbacon <julien.bacon.1@gmail.com>
2025-01-05 11:32:40 -0500

    minimum app running

---
+++ src/pkgs/ui/appComposer.py
@@ -0,0 +1,28 @@
+import logging
+import sys
+
+from PySide6.QtWidgets import QApplication
+
+from .windows import AppWindow
+
+
+class AppComposer:
+    """
+    Application Composer.
+    """
+    def __init__(self) -> None:
+        """
+        Constructor.
+        """
+        self._logger = logging.getLogger('app.composer')
+        self._logger.info('creating Qt app')
+        self._app = QApplication(sys.argv)
+        self._logger.debug('creating UI')
+        self._appWindow = AppWindow()
+
+    def run(self):
+        """
+        Run the application.
+        """
+        self._appWindow.show()
+        sys.exit(self._app.exec_())
