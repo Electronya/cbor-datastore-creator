127f7311cf3986f47bb63f2baf087005e7be5871
jbacon <julien.bacon.1@gmail.com>
2025-01-12 07:07:47 -0500

    adding the signed integer object

--- src/pkgs/objects/__init__.py
+++ src/pkgs/objects/__init__.py
@@ -1,3 +1,3 @@
 from .objectCommon import LimitError, SizeError                     # noqa: E501 F401
+from .signedInteger import SignedInteger, SignedIntegerData         # noqa: E501 F401
 from .unsignedInteger import UnsignedInteger, UnsignedIntegerData   # noqa: E501 F401
-from .objectCommon import LimitError, SizeError                     # noqa: E501 F401
