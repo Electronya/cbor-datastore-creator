d3ddc9c1ad9efaf73f3c48b22bd758cc2aac4706
jbacon <julien.bacon.1@gmail.com>
2025-01-18 22:02:10 -0500

    adding the float type

--- src/pkgs/objects/__init__.py
+++ src/pkgs/objects/__init__.py
@@ -1,3 +1,4 @@
+from .floatObject import Float, FloatData                           # noqa: E501 F401
 from .objectCommon import LimitError, SizeError                     # noqa: E501 F401
 from .signedInteger import SignedInteger, SignedIntegerData         # noqa: E501 F401
 from .unsignedInteger import UnsignedInteger, UnsignedIntegerData   # noqa: E501 F401
