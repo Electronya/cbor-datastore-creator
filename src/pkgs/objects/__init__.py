8e5672858f3f778e7be941a7c029c02b36e498cf
jbacon <julien.bacon.1@gmail.com>
2025-01-19 09:59:34 -0500

    adding the multi-state object

--- src/pkgs/objects/__init__.py
+++ src/pkgs/objects/__init__.py
@@ -1,5 +1,6 @@
 from .buttonState import ButtonState, ButtonStateData, ButtonStateEnum      # noqa: E501 F401
 from .floatObject import Float, FloatData                                   # noqa: E501 F401
+from .multiState import MultiState, MultiStateData                          # noqa: E501 F401
 from .objectCommon import LimitError, SizeError, TimeError                  # noqa: E501 F401
 from .signedInteger import SignedInteger, SignedIntegerData                 # noqa: E501 F401
 from .unsignedInteger import UnsignedInteger, UnsignedIntegerData           # noqa: E501 F401
