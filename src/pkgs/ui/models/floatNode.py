96a517bf845e53fc243c444488dc0b7f748a4fcb
jbacon <julien.bacon.1@gmail.com>
2025-04-02 21:35:53 -0400

    adding getter + setter for float node data

--- src/pkgs/ui/models/floatNode.py
+++ src/pkgs/ui/models/floatNode.py
@@ -20,6 +20,60 @@ class FloatNode(BaseNode):
         super().__init__(name, NodeType.FLOAT, parent=parent)
         self._data = data
 
+    def getMinimum(self) -> float:
+        """
+        Get the minimum value.
+
+        Return
+            The minimum value of the float.
+        """
+        return self._data.min
+
+    def setMinimum(self, min: float) -> None:
+        """
+        Set the minimum value.
+
+        Param
+            min: The minimum value of the float.
+        """
+        self._data.min = min
+
+    def getMaximum(self) -> float:
+        """
+        Get the maximum value.
+
+        Return
+            The maximum value of the float.
+        """
+        return self._data.max
+
+    def setMaximum(self, max: float) -> None:
+        """
+        Set the maximum value.
+
+        Param
+            min: The maximum value of the float.
+        """
+        self._data.max = max
+
+    def getDefault(self) -> float:
+        """
+        Get the default value.
+
+        Return
+            The default value of the float.
+        """
+        return self._data.default
+
+    def setDefault(self, default: float) -> None:
+        """
+        Set the default value.
+
+        Param
+            min: The default value of the float.
+        """
+        self._data.default = default
+
 
 @dataclass
 class FloatArrayElement:
