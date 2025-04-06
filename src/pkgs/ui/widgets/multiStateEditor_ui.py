44057316df3060498874a114799286c0e230d64b
jbacon <julien.bacon.1@gmail.com>
2025-04-06 07:46:20 -0400

    switching from ListView to TableView for the state list

--- src/pkgs/ui/widgets/multiStateEditor_ui.py
+++ src/pkgs/ui/widgets/multiStateEditor_ui.py
@@ -15,8 +15,9 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGridLayout, QLabel, QListView,
-    QPushButton, QSizePolicy, QSpacerItem, QWidget)
+from PySide6.QtWidgets import (QApplication, QGridLayout, QHeaderView, QLabel,
+    QPushButton, QSizePolicy, QSpacerItem, QTableView,
+    QWidget)
 
 class Ui_MultiStateEditor(object):
     def setupUi(self, MultiStateEditor):
@@ -28,20 +29,6 @@ class Ui_MultiStateEditor(object):
         MultiStateEditor.setFont(font)
         self.gridLayout = QGridLayout(MultiStateEditor)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.pbDeleteState = QPushButton(MultiStateEditor)
-        self.pbDeleteState.setObjectName(u"pbDeleteState")
-        icon = QIcon(QIcon.fromTheme(QIcon.ThemeIcon.EditDelete))
-        self.pbDeleteState.setIcon(icon)
-
-        self.gridLayout.addWidget(self.pbDeleteState, 2, 1, 1, 1)
-
-        self.pbAddState = QPushButton(MultiStateEditor)
-        self.pbAddState.setObjectName(u"pbAddState")
-        icon1 = QIcon(QIcon.fromTheme(QIcon.ThemeIcon.ContactNew))
-        self.pbAddState.setIcon(icon1)
-
-        self.gridLayout.addWidget(self.pbAddState, 2, 0, 1, 1)
-
         self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
 
         self.gridLayout.addItem(self.horizontalSpacer_2, 2, 2, 1, 1)
@@ -50,19 +37,33 @@ class Ui_MultiStateEditor(object):
 
         self.gridLayout.addItem(self.horizontalSpacer, 0, 2, 1, 1)
 
-        self.lvStateList = QListView(MultiStateEditor)
-        self.lvStateList.setObjectName(u"lvStateList")
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
 
-        self.gridLayout.addWidget(self.lvStateList, 1, 0, 1, 3)
+        self.gridLayout.addItem(self.verticalSpacer, 3, 1, 1, 1)
+
+        self.pbAddState = QPushButton(MultiStateEditor)
+        self.pbAddState.setObjectName(u"pbAddState")
+        icon = QIcon(QIcon.fromTheme(QIcon.ThemeIcon.ContactNew))
+        self.pbAddState.setIcon(icon)
+
+        self.gridLayout.addWidget(self.pbAddState, 2, 0, 1, 1)
+
+        self.pbDeleteState = QPushButton(MultiStateEditor)
+        self.pbDeleteState.setObjectName(u"pbDeleteState")
+        icon1 = QIcon(QIcon.fromTheme(QIcon.ThemeIcon.EditDelete))
+        self.pbDeleteState.setIcon(icon1)
+
+        self.gridLayout.addWidget(self.pbDeleteState, 2, 1, 1, 1)
 
         self.lblStateList = QLabel(MultiStateEditor)
         self.lblStateList.setObjectName(u"lblStateList")
 
         self.gridLayout.addWidget(self.lblStateList, 0, 0, 1, 2)
 
-        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+        self.tvStateList = QTableView(MultiStateEditor)
+        self.tvStateList.setObjectName(u"tvStateList")
 
-        self.gridLayout.addItem(self.verticalSpacer, 3, 1, 1, 1)
+        self.gridLayout.addWidget(self.tvStateList, 1, 0, 1, 3)
 
 
         self.retranslateUi(MultiStateEditor)
@@ -72,8 +73,8 @@ class Ui_MultiStateEditor(object):
 
     def retranslateUi(self, MultiStateEditor):
         MultiStateEditor.setWindowTitle(QCoreApplication.translate("MultiStateEditor", u"Form", None))
-        self.pbDeleteState.setText("")
         self.pbAddState.setText("")
+        self.pbDeleteState.setText("")
         self.lblStateList.setText(QCoreApplication.translate("MultiStateEditor", u"State List:", None))
     # retranslateUi
 
