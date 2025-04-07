a439e15ad36b1e04c35e51ebce54fe193e45d795
jbacon <julien.bacon.1@gmail.com>
2025-04-07 18:13:50 -0400

    basic editor UI initialization

--- src/pkgs/ui/widgets/multiStateEditor.py
+++ src/pkgs/ui/widgets/multiStateEditor.py
@@ -4,7 +4,7 @@ import PySide6.QtCore as qtc
 import PySide6.QtWidgets as qtw
 
 from .multiStateEditor_ui import Ui_MultiStateEditor
-from ..models import MultiStateNode
+from ..models import MultiStateNode, StateListModel
 
 
 class MultiStateEditor(qtw.QWidget, Ui_MultiStateEditor):
@@ -26,4 +26,32 @@ class MultiStateEditor(qtw.QWidget, Ui_MultiStateEditor):
     def _initUi(self) -> None:
         """
         Initialize the UI connecting signals and slots.
+        """
+        model = StateListModel(self._multiState.getStateList())
+        self.tvStateList.setModel(model)
+        self.cbDefaultState.setModel(model)
+        self.cbDefaultState.setCurrentIndex(self._multiState.getDefaultIndex())
+        self.cbDefaultState.currentIndexChanged.connect(self._selectNewDefault)
+        self.pbAddState.clicked.connect(self._addState)
+        self.pbDeleteState.clicked.connect(self._deleteState)
+
+    @qtc.Slot()
+    def _selectNewDefault(self, default: int) -> None:
+        """
+        Select a new default state.
+
+        Param
+            default: The new default index.
+        """
+
+    @qtc.Slot()
+    def _addState(self) -> None:
+        """
+        Add a new state.
+        """
+
+    @qtc.Slot()
+    def _deleteState(self) -> None:
+        """
+        Delete the selected state.
         """
--- tests/unit/pkgs/ui/widgets/test_MultiStateEditor.py
+++ tests/unit/pkgs/ui/widgets/test_MultiStateEditor.py
@@ -19,6 +19,8 @@ class TestMultiStateEditor(TestCase):
         """
         self._QWidget = 'pkgs.ui.widgets.multiStateEditor.qtw.QWidget.__init__'
         self._loggingMod = 'pkgs.ui.widgets.multiStateEditor.logging'
+        self._StateListModelCls = 'pkgs.ui.widgets.multiStateEditor.' \
+            'StateListModel'
         self._logger = Mock()
         self._multiState = Mock()
         with patch(self._loggingMod) as mockedLoggingMod, \
@@ -27,6 +29,10 @@ class TestMultiStateEditor(TestCase):
                 patch.object(MultiStateEditor, '_initUi'):
             mockedLoggingMod.getLogger.return_value = self._logger
             self._uut = MultiStateEditor(self._multiState)
+            self._uut.tvStateList = Mock()
+            self._uut.cbDefaultState = Mock()
+            self._uut.pbAddState = Mock()
+            self._uut.pbDeleteState = Mock()
 
     def test_constructorGetLogger(self) -> None:
         """
@@ -52,3 +58,26 @@ class TestMultiStateEditor(TestCase):
             uut = MultiStateEditor(multiState)
             mockedSetupUi.assert_called_once_with(uut)
             self.assertEqual(multiState, uut._multiState)
+
+    def test_initUiInitAndConnect(self) -> None:
+        """
+        The _initUi method must initialize the state model, initialize the
+        table view and the combo box and connect the button clicked signals.
+        """
+        states = Mock()
+        defaultState = Mock()
+        model = Mock()
+        with patch(self._StateListModelCls) as mockedModel:
+            mockedModel.return_value = model
+            self._multiState.getStateList.return_value = states
+            self._multiState.getDefaultIndex.return_value = defaultState
+            self._uut._initUi()
+            mockedModel.assert_called_once_with(states)
+            self._uut.tvStateList.setModel.assert_called_once_with(model)
+            self._uut.cbDefaultState.setModel.assert_called_once_with(model)
+            self._uut.cbDefaultState.setCurrentIndex \
+                .assert_called_once_with(defaultState)
+            self._uut.cbDefaultState.currentIndexChanged.connect \
+                .assert_called_once_with(self._uut._selectNewDefault)
+            self._uut.pbAddState.clicked.connect(self._uut._addState)
+            self._uut.pbDeleteState.clicked.connect(self._uut._deleteState)
